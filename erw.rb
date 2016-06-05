91a0c1a2ad8957f6c861410f7ad7aac795c89320
Adam Kinson <adam.kinson@gmail.com>
2016-06-05 15:58:45 -0400

    embed videos with partial

--- erw.rb
+++ erw.rb
@@ -11,12 +11,21 @@ end
 
 
 get "/" do
-  # instance vars here for music & videos
   @music_tracks = [
     {id: 267210344, title: "Suleika I, Franz Schubert", notes: "", recorded_date: Date.new(2013,10,6)},
     {id: 267210343, title: "Twelve Poems of Emily Dickinson: 9. I Felt a Funeral in my Brain, Aaron Copland", notes: "", recorded_date: Date.new(2013,10,6)},
     {id: 267210342, title: "Duex Poemes de Louis Aragon - C, Francis Poulenc", notes: "", recorded_date: Date.new(2013,10,6)},
     {id: 267210340, title: "In Stiller Nacht, Johannes Brahms", notes: "", recorded_date: Date.new(2011,4,23)}
   ]
+
+  @videos = [
+    {id: "jQMrRBnZYlE", title: "Fly Duet from Orphée aux Enfers (Offenbach)", notes: ""},
+    {id: "fIEnps5LIMk", title: "Valse Tyrolienne (Offenbach)", notes: ""},
+    {id: "viM03Q4J04k", title: "C (Poulenc)", notes: ""},
+    {id: "0mutWhcCo0c", title: "Apparition (Debussy)", notes: ""},
+    {id: "iPZ3lV88iRQ", title: "Soleil, fuis de ces lieux (Rameau)", notes: ""},
+    {id: "nfqpNO-icJw", title: "Le Papillon et La Fleur (Fauré)", notes: ""}
+  ]
+
   erb :home
 end
